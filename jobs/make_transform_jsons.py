abe4f965e908a39bd454561c10d5ec3accda8343
stanibor <stani.bork@gmail.com>
2022-12-12 15:15:15 +0100

    simpler augmentations

--- jobs/make_transform_jsons.py
+++ jobs/make_transform_jsons.py
@@ -1,13 +1,18 @@
 from os import PathLike
 from pathlib import Path
+from typing import Tuple, Optional
+
 from fire import Fire
 
 import albumentations as A
 from facer.datasets.transforms import spatial_transforms, photometric_transforms, TO_TENSOR_TRANSFORM
 
 
-def make_transform_json(crop_size, output_size, output: PathLike[str], photometric_json: PathLike[str] = None):
-    s_transform = spatial_transforms((output_size, crop_size), (output_size, output_size))
+def make_transform_json(crop_min_max: Tuple[int, int],
+                        output_size: int,
+                        output: PathLike,
+                        photometric_json: Optional[PathLike] = None):
+    s_transform = spatial_transforms(crop_min_max, (output_size, output_size))
     p_transform = photometric_transforms() if photometric_json is None else A.load(photometric_json)
     transform = A.Compose([*s_transform, *p_transform, *TO_TENSOR_TRANSFORM])
     A.save(transform, output)
